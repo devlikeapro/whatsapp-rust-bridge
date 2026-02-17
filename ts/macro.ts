3f24a26ac551f2944d7ddde912710922026c287a
devlikepro <waha@devlike.pro>
2026-02-17 18:39:22 +0700

    Use sync function to load wasm

--- ts/macro.ts
+++ ts/macro.ts
@@ -1,5 +1,7 @@
-export const base64Wasm = async () => {
-  const bytes = await Bun.file("./pkg/whatsapp_rust_bridge_bg.wasm").bytes();
+import {readFileSync} from "fs";
+
+export const base64Wasm = () => {
+  const bytes = readFileSync("./pkg/whatsapp_rust_bridge_bg.wasm")
 
   const base64 = bytes.toBase64();
 
